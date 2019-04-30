f969187b9ecadefabefd215292182eafdf44f761
Matthew Gilliard <matthew@gilliard.lol>
2019-04-30 20:23:54 +0100

    moar owl pics

--- src/main/java/lol/gilliard/OwlPics.java
+++ src/main/java/lol/gilliard/OwlPics.java
@@ -17,7 +17,23 @@ public class OwlPics {
 
             new PicWithAttribution(
                     "603px-Athene_cuniculariaa.jpg",
-                    "https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Athene_cuniculariaa.jpg/603px-Athene_cuniculariaa.jpg")
+                    "https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Athene_cuniculariaa.jpg/603px-Athene_cuniculariaa.jpg"),
+
+            new PicWithAttribution(
+                    "_wp-content_uploads_0638551344d3981efbe56e0108ae4d458fa21c5e1382721127-s-thumbnail2.jpg",
+                    "http://animalch.net/article/464938162.htm"),
+
+            new PicWithAttribution(
+                    "_wp-content_uploads_b340a3c9a9cac6f9c8c517cd333845e4f3d7eb1b1382721145-s-thumbnail2.jpg",
+                    "http://animalch.net/article/464938162.htm"),
+
+            new PicWithAttribution(
+                    "owlhead.jpg",
+                    "http://animalch.net/article/464938162.htm"),
+
+            new PicWithAttribution(
+                    "weighing.jpg",
+                    "http://animalch.net/article/464938162.html")
 
     );
 
